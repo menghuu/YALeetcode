47ad1862ff6af46ac9178e76bd601ce5aebe36f1
Hu Meng <m@meng.hu>
2020-07-11 22:39:28 +0800

    feat: add acwing 3. 完全背包问题

---
+++ python3/3. 完全背包问题.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 m <m@meng.hu>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+import sys
+
+N, V = sys.stdin.readline().strip().split()
+N, V = int(N), int(V)
+
+vs, ws = [], []
+while True:
+    line = sys.stdin.readline().strip()
+    if line == '':
+        break
+    v, w = line.split()
+    vs.append(int(v))
+    ws.append(int(w))
+
+dps = [0 for _ in range(V+1)]
+
+for i, v, w in zip(range(N), vs, ws):
+    for j in range(v, V+1):
+        jj = j - v
+        dps[j] = max(dps[j], dps[jj] + w)
+
+print(dps[-1])
