a9a76d3d494c45746ef0b0c67d5d433c8ab37247
Hu Meng <m@meng.hu>
2020-07-20 21:31:18 +0800

    feat: 又写了67题，67. 二进制求和

--- python3/67. 二进制求和.py
+++ python3/67. 二进制求和.py
@@ -12,7 +12,8 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         carry = 0
-        res = []
+        ans = ''
+
         for k in range(max(len(a), len(b))):
             i = len(a) - k - 1
             j = len(b) - k - 1
@@ -20,10 +21,14 @@ class Solution:
             n1 = int(a[i]) if i >= 0 else 0
             n2 = int(b[j]) if j >= 0 else 0
 
-            sum = (n1 + n2 + carry)
-            carry = sum // 2
-            sum = sum % 2
-            res.insert(0, str(sum))
-        if carry == 1:
-            res.insert(0, '1')
-        return ''.join(res)
+            s = n1 + n2 + carry
+            
+            s, carry = s % 2, s // 2
+
+            ans = str(s) + ans
+        
+        if carry:
+            ans = '1' + ans
+        
+        return ans
+
