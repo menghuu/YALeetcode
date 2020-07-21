d02e44452e8b815c2119204519cc8bac2ac625db
Hu Meng <m@meng.hu>
2020-07-21 19:45:25 +0800

    fix: better 122. 买卖股票的最佳时机 II

--- python3/122. 买卖股票的最佳时机 II.py
+++ python3/122. 买卖股票的最佳时机 II.py
@@ -11,8 +11,24 @@
 """
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        ans = 0
-        for i in range(1, len(prices)):
-            if prices[i] > prices[i - 1]:
-                ans += prices[i] - prices[i - 1]
-        return ans
+        
+        not_keep_profit = 0
+        keep_profit = -prices[0]
+        for price in prices[1:]:
+            not_keep_profit = max(not_keep_profit, keep_profit + price)
+            keep_profit = max(not_keep_profit - price, keep_profit)
+        return not_keep_profit
+
+
+        dps = [[0, 1] for _ in prices]
+        # dps[i][0]表示第i天的时候，手中没有持有股票，它所能拥有的利润
+        # dps[i][1]表示第i天的时候，手中持有股票，它所能拥有的利润
+
+        for i in range(len(prices)):
+            if i == 0:
+                dps[i][0] = 0
+                dps[i][1] = -prices[i]
+            else:
+                dps[i][0] = max(dps[i - 1][0], dps[i - 1][1] + prices[i])
+                dps[i][1] = max(dps[i - 1][1], dps[i - 1][0] - prices[i])
+        return dps[-1][0]
