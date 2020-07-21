0bce676a91f28e9a245db531211222efc288d1ea
Hu Meng <m@meng.hu>
2020-07-21 18:43:33 +0800

    feat: add 122. 买卖股票的最佳时机 II

---
+++ python3/122. 买卖股票的最佳时机 II.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 m <m@meng.hu>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        ans = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                ans += prices[i] - prices[i - 1]
+        return ans
